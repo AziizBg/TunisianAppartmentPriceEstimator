a433ce5d659b61f9eba266be7306503d16c5f870
zouaghista <59181866+zouaghista@users.noreply.github.com>
2025-02-07 21:57:58 +0100

    Added report generation script

--- src/data/BathroomsNullValues.py
+++ src/data/BathroomsNullValues.py
@@ -2,8 +2,8 @@ import json
 import statistics
 
 # Load the dataset
-file_path = "../../data/RAW/binded.json"
-output_file_path = "updated_binded.json"
+file_path = "../../data/PreProcessed/Treated.json"
+output_file_path = "../../data/PreProcessed/Treated.json"
 
 with open(file_path, "r", encoding="utf-8") as file:
     data = json.load(file)
